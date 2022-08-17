0cd8840fa0d6c6ad556de695367ecc1fb800d7a4
linKe <ly55421@163.com>
2022-08-17 19:08:05 +0800

    替换

---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,103 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  { text: '运维',link: '/ops/',
+    items: [
+      { text: '最佳实践', link: '/pages/027a17/'},
+      { text: '迎刃而解', link: '/pages/29.html'},
+      { text: 'Nginx', link: '/pages/930.html'},
+      { text: 'Php', link: '/pages/2782.html'},
+      { text: 'Zabbix', link: '/pages/596.html'},
+      { text: 'Prometheus', link: '/pages/2468.html'},
+      { text: 'Grafana', link: '/pages/5380.html'},
+      { text: 'CentOS', link: '/pages/5279.html'},
+      { text: 'Systemd', link: '/pages/2501.html'},
+      { text: 'Docker', link: '/pages/1847.html'},
+      { text: 'Rancker', link: '/pages/2671.html'},
+      { text: 'Ansible', link: '/pages/5173.html'},
+      { text: 'Ldap', link: '/pages/3410.html'},
+      { text: 'Gitlab', link: '/pages/243.html'},
+      { text: 'GitHub', link: '/pages/226388/'},
+      { text: 'Etcd', link: '/pages/4050.html'},
+      { text: 'Consul', link: '/pages/f61d95/'},
+      { text: 'RabbitMQ', link: '/pages/2930.html'},
+      { text: 'Kafka', link: '/pages/2951.html'},
+      { text: 'MySql', link: '/pages/1497.html'},
+      { text: 'MongoDB', link: '/pages/2972.html'},
+      { text: 'OpenVPN', link: '/pages/3807.html'},
+      { text: 'KVM', link: '/pages/114.html'},
+      { text: 'VMware', link: '/pages/1248.html'},
+      { text: 'Other', link: '/pages/285.html'},
+    ]
+  },
+  { text: '专题',link: '/topic/',
+    items: [
+      { text: 'ELK', link: '/pages/2351.html'},
+      { text: 'K8S', link: '/pages/1742.html'},
+      { text: 'Nexus', link: '/pages/45c5da/'},
+      { text: 'Jenkins', link: '/pages/2415.html'},
+    ]
+  },
+  { text: '生活',link: '/life/',
+    items: [
+      { text: '随写编年', link: '/pages/2714.html'},
+      { text: '家人物语', link: '/pages/2442.html'},
+      { text: '追忆青春', link: '/pages/fdb9f6/'},
+      { text: '父亲的朋友圈', link: '/pages/3993f9/'},
+      { text: '电影音乐', link: '/pages/184.html'},
+      { text: '效率工具', link: '/pages/136.html'},
+      { text: '博客相关', link: '/pages/844.html'},
+    ]
+  },
+  { text: '编程',link: '/code/',
+    items: [
+      { text: 'Vue', link: '/pages/5164.html'},
+      { text: 'Shell', link: '/pages/531.html'},
+      { text: 'Golang学习笔记', link: '/pages/aeab08/'},
+      { text: 'Golang编程技巧', link: '/pages/c13b45/'},
+    ]
+  },
+  { text: '周刊', link: '/pages/11c668/',
+    items: [
+      { text: '学习周刊', link: '/pages/11c668/'},
+      { text: 'Obsidian插件周刊', link: '/pages/bcc523/'},
+    ]
+  },
+  { text: '关于', link: '/about/' },
+  { text: '索引', link: '/categories/',
+  items: [
+    { text: '分类', link: '/categories/' },
+    { text: '标签', link: '/tags/' },
+    { text: '归档', link: '/archives/' },
+  ],
+},
+{
+  text: '页面',
+  link: '/friends/',
+  items: [
+    {
+      items: [
+        { text: '友链', link: '/friends/' },
+        { text: '导航', link: '/nav/' },
+        { text: '打赏', link: '/reward/' },
+      ]
+    },
+    {
+      items: [
+        { text: 'json2go', link: 'http://public.eryajf.net/json2go' },
+        { text: '微信MD编辑', link: 'http://public.eryajf.net/mdnice' },
+        { text: '国内镜像', link: 'http://public.eryajf.net/mirror' },
+        { text: '出口IP查询', link: 'http://ip.eryajf.net' },
+      ]
+    },
+    {
+      items: [
+        { text: 'ldapdoc', link: 'http://ldapdoc.eryajf.net' },
+        { text: 'HowToStartOpenSource', link: 'https://eryajf.github.io/HowToStartOpenSource' },
+        { text: 'vdoing-template', link: 'https://eryajf.github.io/vdoing-template/' },
+      ]
+    }
+  ],
+},
+{ text: '开往', link: 'https://travellings.link' },
+]
