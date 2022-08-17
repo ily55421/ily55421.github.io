35e5dc7e9cbf760c84fe096b9d7631e35bc11aad
ily55421 <ly55421@163.com>
2022-08-17 20:32:33 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,20 +2,45 @@ const head = require('./config/head.js');
 const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
-
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
-
-  title: "二丫讲梵",
-  description: '💻学习📝记录🔗分享\n学无止境是永远前进的基础，跃然纸上是对知识的总结交代，与众分享则是实现价值的最好方式。',
-  base: '/', // 格式：'/<仓库名>/'， 默认'/'
+  title: "vdoing-template",
+  description: 'vdoing博客主题模板',
+  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
-    extractHeaders: [ 'h2', 'h3', 'h4', 'h5', 'h6' ]
   },
 
   head,
   plugins,
   themeConfig,
+
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "adb9fb0ac1159e00ce7f",
+        clientSecret: "27da8dc85f808c2bd1b6e44da5ae69c4ddf17d8d",
+        owner: "eryajf",
+        repo: "vdoing-template",
+      },
+    ],
+    // 增强 markdown
+    [
+      "md-enhance", {
+        // 启用 TeX 支持
+        tex: true,
+        // Enable mermaid
+        mermaid: true,
+        // 启用流程图
+        flowchart: true,
+      },
+    ],
+  ],
 }
