c3bca3e6c22ff014b36ce74f5842d405c6308325
ily55421 <ly55421@163.com>
2022-08-17 20:52:51 +0800

    s

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "ily55421.github.io",
+  title: "ily55421",
   description: 'vdoing博客主题模板',
-  base: '/ily55421.github.io/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
