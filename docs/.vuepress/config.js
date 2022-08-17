2dd44c5a007f59fb26e8b79eb52ce258847f811d
ily55421 <ly55421@163.com>
2022-08-17 20:38:25 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "ily55421.github.io",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/ily55421.github.io/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -25,10 +25,10 @@ module.exports = {
         needComments: true,
         // 其他的 Vssue 配置
         autoCreateIssue: true,
-        clientId: "adb9fb0ac1159e00ce7f",
-        clientSecret: "27da8dc85f808c2bd1b6e44da5ae69c4ddf17d8d",
-        owner: "eryajf",
-        repo: "vdoing-template",
+        clientId: "cf9ec197dde00bb3c601",
+        clientSecret: "36e0127a9d6380d2f3e1cedb573e264df86417c3",
+        owner: "ily55421",
+        repo: "ily55421.github.io",
       },
     ],
     // 增强 markdown
